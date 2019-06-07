bc4360cbb0d683a69ee86ad22dffdfde57c8c75f
Pedro Rafael <pedro.rafael.marinho@gmail.com>
2019-06-07 10:18:08 -0300

    Primeiro commit

---
+++ code.R
@@ -0,0 +1,25 @@
+# Consideration of reviewers. ---------------------------------------------
+
+  # Reviewer #2: The paper describes an R package (AdequacyModel) that uses particle swarm optimization
+  # (PSO) as a tool for general purpose optimization and for deriving maximum likelihood estimates for the
+  # parameters of probability distributions. The package also presents some goodness-of-fit tests. Such a 
+  # computational library is of great interest for the statistical, mathematical, engineering communities,
+  # the paper is generally well written and organized. However, the results and examples provided in the manuscript
+  # do not clearly demonstrate the differences (advantages / disadvantages) of AdequacyModel when compared to other
+  # available packages (e.g. "pso") and PSO methods. Paper contribution should be clearly stated and supported by the
+  # results. The following issues should be addressed by the authors:
+  
+  # In portuguese (native language): 
+  
+  # O documento descreve um pacote de R (AdequacyModel) que utiliza optimização enxame de partículas
+  # (PSO) como uma ferramenta para a optimização de uso geral e para derivar estimativas de 
+  # probabilidade máxima para os parâmetros de distribuições de probabilidade. O pacote também 
+  # apresenta alguns testes bondade-de-ajuste. Tal biblioteca computacional é de grande interesse para
+  # as comunidades, engenharia estatísticos, matemáticos, o papel é geralmente bem escrito e organizado.
+  # No entanto, os resultados e exemplos fornecidos em manuscrito não demonstram claramente as diferenças
+  # (vantagens / desvantagens) de AdequacyModel quando comparado com outros pacotes disponíveis (por exemplo, "PSO")
+  # e métodos de OSP. contribuição do papel deve ser claramente indicado e apoiado pelos resultados.
+  
+
+
+
