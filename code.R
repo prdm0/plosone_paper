9987c09112326b7ac76fba118c4b034ad8fadaa5
prdm0 <pedro.rafael.marinho@gmail.com>
2019-07-04 13:34:24 -0300

    Update code

--- code.R
+++ code.R
@@ -20,8 +20,6 @@
 # (vantagens / desvantagens) de AdequacyModel quando comparado com outros pacotes disponíveis (por exemplo, "PSO")
 # e métodos de OSP. contribuição do papel deve ser claramente indicado e apoiado pelos resultados.
 
-
-
 library(tidyverse)
 
 
@@ -40,35 +38,46 @@ rastrigin <- function(par, x, A = 10L) {
   return(A * length(x_vector) + sum(x_vector ^ 2 - A * cos(2 * pi * x_vector)))
 }
 
-# Test (BFGS, AdequacyModel, pso) -----------------------------------------
+# Monte Carlo (PSO of the AdequacyModel package) --------------------------
 
-optim(par = c(1, 1, 1), fn = rastrigin, method = "BFGS")
+library(parallel) # OpenMP.
+library(AdequacyModel)
 
-set.seed(0)
-
-AdequacyModel::pso(func = rastrigin, S = 250, lim_inf = rep(-5.12, 3),
-                   lim_sup = rep(5.12, 3), e = 0.1, N = 1000, prop = 0.2)
-
-pso::psoptim(rep(NA,3), rastrigin,
-        lower=-5.12,upper=5.12)
-
-# Earsom Function: --------------------------------------------------------
-
-earsom <- function(par, x){
-  x <- par[1]
-  y <- par[2]
-
-  -cos(x) * cos(y) * exp(-(x - pi)^2 - (y - pi)^2)
+onestep <- function(x, list_args) {
+  result <- do.call("pso", args = list_args)
+  list(par = result$par, value = result$f[length(result$f)])
 }
 
-# Test (BFGS, AdequacyModel, pso) -----------------------------------------
+args <- list(
+  func = rastrigin,
+  S = 150,
+  lim_inf = rep(-5.12, 2),
+  lim_sup = rep(5.12, 2),
+  e = 0.0001,
+  N = 30L,
+  prop = 0.05
+)
 
-optim(par = c(1, 1), fn = earsom, method = "BFGS")
+# onestep(list_args = args)
 
-set.seed(0)
+# A ‘combined multiple-recursive generator’ from L'Ecuyer (1999), each element of which is a feedback
+# multiplicative generator with three integer elements: thus the seed is a (signed) integer vector of
+# length 6. The period is around 2^191.
 
-result <- AdequacyModel::pso(func = earsom, S = 5000, lim_inf = rep(-100, 2),
-                   lim_sup = rep(100, 2), e = 1e-8, N = 1000, prop = 0.4)
+set.seed(seed = 1L, kind = "L'Ecuyer-CMRG")
 
-pso::psoptim(rep(NA,2), earsom,
-             lower=-100,upper=100)
+result <-
+  mclapply(
+    X = 1:100L,
+    FUN = onestep,
+    mc.cores = detectCores(),
+    list_args = args
+  ) # Parallel lapply function.
+
+result <- unlist(result)
+par_1 <- result[names(result) == "par1"]
+par_2 <- result[names(result) == "par2"]
+value <- result[names(result) == "value"]
+
+
+
